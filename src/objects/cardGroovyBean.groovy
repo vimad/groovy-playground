613c6cd366f505b90d93d65e0281acb0e1474f04
vinod.mby@gmail.com <vinod madubashana>
2023-06-18 15:24:55 +0530

    object examples

---
+++ src/objects/cardGroovyBean.groovy
@@ -0,0 +1,6 @@
+package objects
+
+class cardGroovyBean {
+    String name
+    int num_digits
+}
---
+++ src/objects/swipability.groovy
@@ -0,0 +1,13 @@
+package objects
+
+trait swipability {
+    int num_swipes = 0
+    void validateSwipe(){
+        println("Validating Swipe")
+    }
+    int getSwipes(){
+        num_swipes+=1
+        num_swipes
+    }
+
+}
